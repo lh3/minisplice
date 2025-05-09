d75fb3a753767f65b4eefb8fb2c7b9ede1d1d0af
Heng Li <lh3@me.com>
2025-05-08 21:59:22 -0400

    modernized gff2bed

--- gff2bed.js
+++ gff2bed.js
@@ -2,6 +2,10 @@
 
 "use strict";
 
+/***************
+ * k8 routines *
+ ***************/
+
 Array.prototype.delete_at = function(i) {
 	for (let j = i; j < this.length - 1; ++j)
 		this[j] = this[j + 1];
@@ -82,26 +86,66 @@ function* k8_readline(fn) {
 	buf.destroy();
 }
 
-function gff2bed(args)
+/*****************
+ * main routines *
+ *****************/
+
+const g2b_colors = {
+	'protein_coding':'0,128,255',
+	'mRNA':'0,128,255',
+	'lincRNA':'0,192,0',
+	'snRNA':'0,192,0',
+	'miRNA':'0,192,0',
+	'misc_RNA':'0,192,0'
+};
+
+class Transcript {
+	constructor(tid, type, ctg, strand, gid, gname) {
+		this.tid = tid, this.type = type, this.ctg = ctg, this.strand = strand, this.gid = gid, this.gname = gname;
+		this.cds_st = -1, this.cds_en = -1;
+		this.exon = [];
+	}
+	row() {
+		const disp_name = [this.tid, this.type, this.gname].join("|");
+		const a = this.exon.sort(function(a,b) {return a.st - b.st});
+		const st = a[0].st;
+		const en = a[a.length - 1].en;
+		const cds_st = this.cds_st >= 0? this.cds_st : st;
+		const cds_en = this.cds_en >= 0? this.cds_en : en;
+		if (cds_st < st || cds_en > en)
+			throw Error(`inconsistent thick start or end for transcript ${this.tid}`);
+		let sizes = [], starts = [];
+		for (let i = 0; i < a.length; ++i) {
+			if (i > 0 && a[i].st <a[i-1].en)
+				throw Error(`overlapping exon for transcript ${this.tid}`);
+			sizes.push(a[i].en - a[i].st);
+			starts.push(a[i].st - st);
+		}
+		let color = g2b_colors[this.type];
+		if (color == null) color = "196,196,196";
+		return [this.ctg, st, en, disp_name, 1000, this.strand, cds_st, cds_en, color, a.length, sizes.join(",")+",", starts.join(",")+","];
+	}
+}
+
+function g2b_print(gene, long_only)
 {
-	let fn_ucsc_fai = null, is_short = false, keep_gff = false, print_junc = false, output_gene = false, ens_canon_only = false;
-	for (const o of getopt(args, "u:sgjGe")) {
-		if (o.opt == 'u') fn_ucsc_fai = o.arg;
-		else if (o.opt == 's') is_short = true;
-		else if (o.opt == 'g') keep_gff = true;
-		else if (o.opt == 'j') print_junc = true;
-		else if (o.opt == 'G') output_gene = true;
-		else if (o.opt == 'e') ens_canon_only = true;
+	for (let i = 0; i < gene.length; ++i)
+		print(gene[i].row().join("\t"));
+}
+
+function g2b_main(args)
+{
+	let fn_ucsc_fai = null, ens_canon_only = false;
+	for (const o of getopt(args, "u:e")) {
+		if (o.opt == '-u') fn_ucsc_fai = o.arg;
+		else if (o.opt == '-e') ens_canon_only = true;
 	}
 
 	if (args.length == 0) {
 		print("Usage: gff2bed.js [options] <in.gff>");
 		print("Options:");
-		print("  -j       output junction BED");
-		print("  -s       print names in the short form");
 		print("  -u FILE  hg38.fa.fai for chr name conversion");
 		print("  -e       only show transcript tagged with 'Ensembl_canonical'");
-		print("  -g       output GFF (used with -u)");
 		return;
 	}
 
@@ -121,118 +165,62 @@ function gff2bed(args)
 		}
 	}
 
-	const colors = {
-		'protein_coding':'0,128,255',
-		'mRNA':'0,128,255',
-		'lincRNA':'0,192,0',
-		'snRNA':'0,192,0',
-		'miRNA':'0,192,0',
-		'misc_RNA':'0,192,0'
-	};
+	const re_gff = /\b(transcript_id|transcript_type|transcript_biotype|gene_name|gene_id|gbkey|tag)( "([^"]+)"|=([^;]+));/g;
+	const re_gff_gene = /\b(gene_id|gene_type|source_gene|gene_biotype|gene_name)( "([^;]+)"|=([^;]+));/g;
 
-	function print_bed12(exons, cds_st, cds_en, is_short, print_junc)
-	{
-		if (exons.length == 0) return;
-		var name = is_short? exons[0][7] + "|" + exons[0][5] : exons[0].slice(4, 7).join("|");
-		var a = exons.sort(function(a,b) {return a[1]-b[1]});
-		if (print_junc) {
-		for (var i = 1; i < a.length; ++i)
-			print(a[i][0], a[i-1][2], a[i][1], name, 1000, a[i][3]);
-			return;
-		}
-		var sizes = [], starts = [], st, en;
-		st = a[0][1];
-		en = a[a.length - 1][2];
-		if (cds_st == 1<<30) cds_st = st;
-		if (cds_en == 0) cds_en = en;
-		if (cds_st < st || cds_en > en)
-			throw Error("inconsistent thick start or end for transcript " + a[0][4]);
-		for (var i = 0; i < a.length; ++i) {
-			sizes.push(a[i][2] - a[i][1]);
-			starts.push(a[i][1] - st);
-		}
-		let color = colors[a[0][5]];
-		if (color == null) color = '196,196,196';
-		print(a[0][0], st, en, name, 1000, a[0][3], cds_st, cds_en, color, a.length, sizes.join(",") + ",", starts.join(",") + ",");
-	}
-
-	const re_gtf  = /\b(transcript_id|transcript_type|transcript_biotype|gene_name|gene_id|gbkey|transcript_name|tag) "([^"]+)";/g;
-	const re_gff3 = /\b(transcript_id|transcript_type|transcript_biotype|gene_name|gene_id|gbkey|transcript_name)=([^;]+)/g;
-	const re_gtf_gene  = /\b(gene_id|gene_type|gene_name) "([^;]+)";/g;
-	const re_gff3_gene = /\b(gene_id|gene_type|source_gene|gene_biotype|gene_name)=([^;]+);/g;
-
-	let exons = [], cds_st = 1<<30, cds_en = 0, last_id = null;
+	let gene = [];
 	for (const line of k8_readline(args[0])) {
 		let t = line.split("\t");
-		if (keep_gff) {
-			if (t[0].charAt(0) != '#' && ens2ucsc[t[0]] != null)
-				t[0] = ens2ucsc[t[0]];
-			print(t.join("\t"));
-			continue;
-		}
 		if (t[0].charAt(0) == '#') continue;
-		if (output_gene) {
-			let id = null, src = null, biotype = null, type = "", name = "N/A";
-			if (t[2] != "gene") continue;
-			while ((m = re_gtf_gene.exec(t[8])) != null) {
-				if (m[1] == "gene_id") id = m[2];
-				else if (m[1] == "gene_type") type = m[2];
-				else if (m[1] == "gene_name") name = m[2];
-			}
-			while ((m = re_gff3_gene.exec(t[8])) != null) {
-				if (m[1] == "gene_id") id = m[2];
-				else if (m[1] == "source_gene") src = m[2];
-				else if (m[1] == "gene_type") type = m[2];
-				else if (m[1] == "gene_biotype") biotype = m[2];
-				else if (m[1] == "gene_name") name = m[2];
-			}
-			if (src != null) id = src;
-			if (type == "" && biotype != null) type = biotype;
-			print(t[0], parseInt(t[3]) - 1, t[4], [id, type, name].join("|"), 1000, t[6]);
-			continue;
-		}
 		if (t[2] != "CDS" && t[2] != "exon") continue;
-		t[3] = parseInt(t[3]) - 1;
-		t[4] = parseInt(t[4]);
-		var id = null, type = "", name = "N/A", biotype = "", m, tname = "N/A", ens_canonical = false;
-		while ((m = re_gtf.exec(t[8])) != null) {
-			if (m[1] == "transcript_id") id = m[2];
-			else if (m[1] == "transcript_type") type = m[2];
-			else if (m[1] == "transcript_biotype" || m[1] == "gbkey") biotype = m[2];
-			else if (m[1] == "gene_name" || m[1] == "gene_id") name = m[2];
-			else if (m[1] == "transcript_name") tname = m[2];
-			else if (m[1] == "tag" && m[2] == "Ensembl_canonical") ens_canonical = true;
+
+		// update contig name if requested
+		let ctg = t[0];
+		if (fn_ucsc_fai != null) {
+			if (ens2ucsc[ctg] != null)
+				tr.ctg = ens2ucsc[ctg];
+			else if (/^[A-Z]+\d+\.\d+$/.test(ctg))
+				ctg = ctg.replace(/([A-Z]+\d+)\.(\d+)/, "chrUn_$1v$2");
 		}
-		while ((m = re_gff3.exec(t[8])) != null) {
-			if (m[1] == "transcript_id") id = m[2];
-			else if (m[1] == "transcript_type") type = m[2];
-			else if (m[1] == "transcript_biotype" || m[1] == "gbkey") biotype = m[2];
-			else if (m[1] == "gene_name" || m[1] == "gene_id") name = m[2];
-			else if (m[1] == "transcript_name") tname = m[2];
+
+		// collect meta annotation
+		const st = parseInt(t[3]) - 1;
+		const en = parseInt(t[4]);
+		let m, tid = null, gid = null, type = null, gname = null, biotype = null, ens_canonical = false;
+		while ((m = re_gff.exec(t[8])) != null) {
+			const key = m[1], val = m[3]? m[3] : m[4];
+			if (key == "transcript_id") tid = val;
+			else if (key == "transcript_type") type = val;
+			else if (key == "transcript_biotype" || key == "gbkey") biotype = val;
+			else if (key == "gene_name") gname = val;
+			else if (key == "gene_id") gid = val;
+			else if (key == "tag" && val == "Ensembl_canonical") ens_canonical = true;
 		}
 		if (ens_canon_only && !ens_canonical) continue;
-		if (type == "" && biotype != "") type = biotype;
-		if (id == null) throw Error("No transcript_id");
-		if (id != last_id) {
-			print_bed12(exons, cds_st, cds_en, is_short, print_junc);
-			exons = [], cds_st = 1<<30, cds_en = 0;
-			last_id = id;
-		}
-		if (t[2] == "CDS") {
-			cds_st = cds_st < t[3]? cds_st : t[3];
-			cds_en = cds_en > t[4]? cds_en : t[4];
-		} else if (t[2] == "exon") {
-			if (fn_ucsc_fai != null) {
-				if (ens2ucsc[t[0]] != null)
-					t[0] = ens2ucsc[t[0]];
-				else if (/^[A-Z]+\d+\.\d+$/.test(t[0]))
-					t[0] = t[0].replace(/([A-Z]+\d+)\.(\d+)/, "chrUn_$1v$2");
+		if (tid == null) throw Error("No transcript_id");
+		if (gname == null) gname = gid? gid : "*"; // infer gene name
+		if (gid == null) gid = gname; // if gene_id missing, use gene name to identify a gene
+		if (type == "" && biotype != "") type = biotype; // infer transcript type
+
+		// finish transcript or gene
+		if (gene.length == 0 || gene[gene.length - 1].tid != tid) { // changing transcript
+			if (gene.length > 0 && gene[gene.length - 1].gid != gid) { // changing gene
+				g2b_print(gene);
+				gene = [];
 			}
-			exons.push([t[0], t[3], t[4], t[6], id, type, name, tname]);
+			gene.push(new Transcript(tid, type, ctg, t[6], gid, gname));
+		}
+
+		// update the last transcript
+		let tr = gene[gene.length - 1];
+		if (t[2] == "CDS") {
+			tr.cds_st = tr.cds_st >= 0 && tr.cds_st < st? tr.cds_st : st;
+			tr.cds_en = tr.cds_en >= 0 && tr.cds_en > en? tr.cds_en : en;
+		} else if (t[2] == "exon") {
+			tr.exon.push({ st:st, en:en });
 		}
 	}
-	if (last_id != null)
-		print_bed12(exons, cds_st, cds_en, is_short, print_junc);
+	g2b_print(gene);
 }
 
-gff2bed(arguments);
+g2b_main(arguments);
